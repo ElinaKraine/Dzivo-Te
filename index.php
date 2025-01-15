e37bb7d41fabc1b17e925b93d8cd7df86d41fd3d
ElinaKraine <elinaliepkr@gmail.com>
2025-01-15 18:35:01 +0200

    Izveidota Sākumlapa

--- index.php
+++ index.php
@@ -4,23 +4,104 @@
 ?>
 
     <section class="galvena">
-
+        <div class="galvenaAttela">
+            <img src="images/galvenaAttela.png">
+        </div>
+        <div class="galvenaisTeksts">
+            <h1>Dzīvo, kur vēlies. <br>Sāciet ar mums!</h1>
+        </div>
     </section>
 
     <section class="galvenaStatistika">
-
+        <div class="kasteInfo">
+            <h1>798</h1>
+            <p>Mājokļi</p>
+        </div>
+        <img src="images/zieds.png">
+        <div class="kasteInfo">
+            <h1>25</h1>
+            <p>Gadu pieredze</p>
+        </div>
+        <img src="images/zieds.png">
+        <div class="kasteInfo">
+            <h1>3000</h1>
+            <p>Laimīgi klienti</p>
+        </div>
+        <img src="images/zieds.png">
+        <div class="kasteInfo">
+            <h1>120</h1>
+            <p>Apbalvojumi</p>
+        </div>
     </section>
 
     <section class="parMajaslapu">
-
+        <div class="parInfo">
+            <div class="kreisaKaste">
+                <a href="parmums.php" class="btn">Par Māju Vieta</a>
+                <h1>Māju Vieta — Jūsu mājas sākas šeit</h1>
+            </div>
+            <div class="labaKaste">
+                <p>Nekustamā īpašuma izīrēšanas un pirkšanas platforma. Atrodiet perfektu māju vai dzīvokli, kurā dzīvot, īrēt vai ērti pārcelties. Ērta meklēšana, aktuāli saraksti un iespēja viegli pieteikt savu mājokli pārdošanai vai īrei. Ar mums Jūsu nekustamā īpašuma meklēšana kļūst vienkārša un ērta! Jūsu jaunās mājas sākas šeit – soli tuvāk sapņu dzīvesvietai.</p>
+            </div>
+        </div>
+        <div class="attelas">
+            <div class="pirmaAttela">
+                <img src="images/parMumsAttela1.png">
+            </div>
+            <div class="otraAttela">
+                <img src="images/parMumsAttela2.png">
+            </div>
+            <div class="tresaAttela">
+                <img src="images/parMumsAttela3.png">
+            </div>
+        </div>
     </section>
 
     <section class="piedavajumi">
-
+        <h1>Skatiet, kā <span>Māju Vieta</span> var palīdzēt</h1>
+        <div class="lielaKaste">
+            <div class="mazaKaste">
+                <img src="images/pirkt.png">
+                <h3>Pirkt mājokli</h3>
+                <p>Mājaslapā ir pieejami vairāk nekā 1 miljons+ pārdodamo māju, tāpēc Māju Vieta var Jums atrast māju, kuru jūs vēlēsieties saukt par savām mājām.</p>
+                <a href="" class="btn">Mājās</a>
+            </div>
+            <div class="mazaKaste">
+                <img src="images/iret.png">
+                <h3>Īrēt mājokli</h3>
+                <p>Izmantojot vairāk nekā 35 filtrus un pielāgotu atslēgvārdu meklēšanu, Māju Vieta var palīdzēt Jums viegli atrast māju vai dzīvokli īrei, kas Jums patiks.</p>
+                <a href="" class="btn">Dzīvokli</a>
+            </div>
+            <div class="mazaKaste">
+                <img src="images/pardot.png">
+                <h3>Pievienot savu sludinājumu</h3>
+                <p id="tresaisTeksts">Jūs varat ātri un ērti piedāvāt savu mājokli pārdošanai vai īrei. Aizpildiet vienkāršu veidlapu, pievienojiet fotogrāfijas un svarīgu informāciju, un Jūsu sludinājums būs pieejams tūkstošiem lietotāju.</p>
+                <a href="" class="btn">Pievienot sludinājumu</a>
+            </div>
+        </div>
     </section>
 
     <footer>
-        
+        <div class="lielaKaste">
+            <div class="mazaKaste">
+                <h1>Māju Vieta</h1>
+                <p>Dzīvo, kur vēlies. <br> Sāciet ar mums!</p>
+            </div>
+            <div class="mazaKaste">
+                <h2>KONTAKTI</h2>
+                <ul>
+                    <li><i></i> Liela iela 10, LV-3414, Liepāja, Latvija</li>
+                    <li><i></i> +371 21228753</li>
+                    <li><i></i> info@majuvieta.lv</li>
+                </ul>
+            </div>
+            <div class="mazaKaste">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-square-instagram"></i>
+                <i class="fa-brands fa-linkedin"></i>
+            </div>
+        </div>
+        <p>&copy; Autortiesības 2025 Māju Vieta - Elīna Kraine</p>
     </footer>
 
 </body>
